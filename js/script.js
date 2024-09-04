1b790cbf6a97666a5d86fdedf974bdbbc0f1202b
Tacoboy <110112143+MorhluM@users.noreply.github.com>
2024-09-04 11:44:21 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+console.log("hello javascript"); //จะไปอยู่ที่หน้าต่างคอนโซล
+console.log("hello Wednesday");
+document.getElementById("text").innerHTML = "Teenoi inwza"; //ไปแสดงหน้าเว็บ
+document.write("Hello document write");
+
+window.alert("welcome to my page"); //แสดงเด้งเป็นกล่องข้อความalert
